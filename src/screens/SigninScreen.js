b5d8785ec6cea5c78aaafbcbed5f9e0dce5f8937
Adrian Grillo <grillo.adrian13@gmail.com>
2021-10-15 14:49:10 -0400

    added components to render on signin screen

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { Context as AuthContext } from '../context/AuthContext'
 import AuthForm from '../components/AuthForm'
+import NavLink from '../components/NavLink'
 
 const SigninScreen = () => {
+    const { state, signin } = React.useContext(AuthContext)
+
     return ( 
         <View style={styles.container}>
-            <Text>Signin Screen</Text>
+            <AuthForm
+            headerText='Sign in'
+            errorMessage={state.errorMessage}
+            onSubmit={signin}
+            submitButtonText='Sign in'
+            />
+            <NavLink 
+                routeName='Signup'
+                text='Dont have an account? Sign up instead'
+            />
         </View>
     )
 }
@@ -13,8 +26,14 @@ const SigninScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        marginBottom: 200
     }
 })
+
+// Disable header 
+SigninScreen.navigationOptions = {
+    header: false
+}
 
 export default SigninScreen
