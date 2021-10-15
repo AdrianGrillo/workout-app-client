fe3895faa0c083f86ca73abff348c1b6237e0ad0
Adrian Grillo <grillo.adrian13@gmail.com>
2021-10-15 14:32:06 -0400

    extracted navigation logic between signin and signup screens to NavLink component

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 import { Context as AuthContext } from '../context/AuthContext'
 import AuthForm from '../components/AuthForm'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import Spacer from '../components/Spacer'
+import NavLink from '../components/NavLink'
 
 const SignupScreen = () => {
     const { state, signup } = React.useContext(AuthContext)
@@ -16,11 +15,10 @@ const SignupScreen = () => {
                 submitButtonText='Sign up'
                 onSubmit={signup}
             />
-            <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
-                <Spacer>
-                    <Text style={styles.link}>Already have an account? Sign in instead</Text>
-                </Spacer>
-            </TouchableOpacity>
+            <NavLink 
+                routeName='Signin' 
+                text='Already have an account? Sign in instead'
+            />
         </View>
     )
 }
@@ -35,9 +33,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         marginBottom: 200
-    },
-    link: {
-        color: 'blue'
     }
 })
 
