92f6a30860750998086bb42f1e5f92b75c028e5b
Adrian Grillo <grillo.adrian13@gmail.com>
2021-10-21 19:46:11 -0400

    TrackCreateScreen now requests permission to access users location data

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://0f9a-47-205-17-247.ngrok.io'
+    baseURL: 'https://de5c-47-205-17-247.ngrok.io'
 })
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -3,12 +3,33 @@ import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Map from '../components/Map'
+import { requestForegroundPermissionsAsync } from 'expo-location'
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = React.useState(null)
+
+    // Ask permission from users device if the app can use thier devices location data
+    const startWatching = async () => {
+        try {
+            const { granted } = await requestForegroundPermissionsAsync()
+
+            if(!granted) {
+                throw new Error('Location permission not granted')
+            }
+        } catch {
+            setErr(e)
+        }
+    }
+
+    React.useEffect(() => {
+        startWatching()
+    }, [])
+
     return (
         <SafeAreaView forceInset={{ top: 'always' }}>
             <Text h2>Create a Track</Text>
             <Map />
+            {err ? <Text>Please enable location services</Text> : null}
         </SafeAreaView>
     ) 
 }
