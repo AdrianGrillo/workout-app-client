c1472e8bdcda8129e4c31a5028061841c99b374a
Adrian Grillo <grillo.adrian13@gmail.com>
2021-10-22 18:24:28 -0400

    TrackCreateScreen now accepts isFocused as props to eventually toggle location tracking

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -3,17 +3,18 @@ import React from 'react'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { withNavigationFocus } from 'react-navigation'
 import Map from '../components/Map'
 import { Context as LocationContext } from '../context/LocationContext'
 import useLocation from '../hooks/useLocation'
 
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({ isFocused }) => {
     // Location context isnt extracted to hook because it's specific to creating a track
     const { addLocation } = React.useContext(LocationContext)
 
     // Pass addLocation as a callback to useLocation, this will be used to update app state with new location data that's being generated from watchLocationAsync inside the hook
     const [err] = useLocation(addLocation)
-
+    
     return (
         <SafeAreaView forceInset={{ top: 'always' }}>
             <Text h2>Create a Track</Text>
@@ -25,4 +26,4 @@ const TrackCreateScreen = () => {
 
 const styles = StyleSheet.create({})
 
-export default TrackCreateScreen
+export default withNavigationFocus(TrackCreateScreen)
