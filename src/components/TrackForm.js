492810781d63603731c74a05f57bded9261ccc0f
Adrian Grillo <grillo.adrian13@gmail.com>
2021-10-23 00:14:36 -0400

    wired TrackForm up to global state

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import { Input, Button } from 'react-native-elements'
 import Spacer from './Spacer'
+import { Context as LocationContext } from '../context/LocationContext'
 
 const TrackForm = () => {
- return <>
-    <Spacer />
-    <Input placeholder='Enter Name' />
-    <Spacer>
-    <Button title='Start Recording' />
-    </Spacer>
- </>
+    const { 
+        state: { name, recording, locations }, 
+        startRecording, 
+        stopRecording, 
+        changeName 
+    } = React.useContext(LocationContext)
+
+    return <>
+        <Spacer />
+        <Input 
+            placeholder='Enter Name' 
+            onChangeText={changeName}
+            value={name}
+        />
+        <Spacer>
+            {recording ? (
+                <Button title={'Stop'} onPress={stopRecording} /> 
+            ) : (
+                <Button title={'Start Recording'} onPress={startRecording} /> 
+            )}
+
+        </Spacer>
+    </>
 }
 
 export default TrackForm
