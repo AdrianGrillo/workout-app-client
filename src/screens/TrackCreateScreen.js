75022b4131141cee6f67fb4768a3051474785fe4
Adrian Grillo <grillo.adrian13@gmail.com>
2021-10-25 18:10:45 -0400

    recording continues after user has navigated away from TrackCreateScreen

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -11,15 +11,15 @@ import TrackForm from '../components/TrackForm'
 
 const TrackCreateScreen = ({ isFocused }) => {
     // Location context isnt extracted to hook because it's specific to creating a track
-    const { state, addLocation } = React.useContext(LocationContext)
+    const { state: { recording }, addLocation } = React.useContext(LocationContext)
 
     // Only give a new version of callback when state.recording changes to avoid a stale reference inside the useEffect hook in useLocation
     const callback = React.useCallback(location => {
-        addLocation(location, state.recording)
-    }, [state.recording])
+        addLocation(location, recording)
+    }, [recording])
 
     // Pass addLocation as a callback to useLocation, this will be used to update app state with new location data that's being generated from watchLocationAsync inside the hook
-    const [err] = useLocation(isFocused, callback)
+    const [err] = useLocation(isFocused || recording, callback)
     
     return (
         <SafeAreaView forceInset={{ top: 'always' }}>
