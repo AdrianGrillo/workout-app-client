3518c5e4349be42ef759d13c39d2bed237d7ec66
Adrian Grillo <grillo.adrian13@gmail.com>
2021-10-26 21:56:44 -0400

    TrackDetailScreen now fetches the track the user pressed on from TrackListScreen

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import { View, StyleSheet, Text } from 'react-native'
+import { Context as TrackContext } from '../context/TrackContext'
 
-const TrackDetailScreen = () => {
-    return <Text style={{ fontSize: 48 }}>TrackDetailScreen</Text>
+const TrackDetailScreen = ({ navigation }) => {
+    const { state } = React.useContext(TrackContext)
+    const _id = navigation.getParam('_id')
+
+    // Find a track with the ID of the one the user pressed on TrackListScreen
+    const track = state.find(t => t._id === _id)
+
+    return <Text style={{ fontSize: 48 }}>{track.name}</Text>
 }
 
 const styles = StyleSheet.create({})
