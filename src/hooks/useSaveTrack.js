ffc810f8db3c4973251a9325f8c6782092caf2ba
Adrian Grillo <grillo.adrian13@gmail.com>
2021-10-26 21:30:43 -0400

    TrackForm now resets and user is navigated to TrackList when they press save track

--- src/hooks/useSaveTrack.js
+++ src/hooks/useSaveTrack.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Context as TrackContext } from '../context/TrackContext'
 import { Context as LocationContext } from '../context/LocationContext'
+import { navigate } from '../navigationRef'
 
 // This hook will take the locations and name properties from LocationContext and pass it to an action function from TrackContext, coordinating information between the two
 export default () => {
     const { createTrack } = React.useContext(TrackContext)
-    const { state: { name, locations } } = React.useContext(LocationContext)
+    const { state: { name, locations }, reset } = React.useContext(LocationContext)
 
-    const saveTrack = () => {
-        createTrack(name, locations)
+    // Reset global state only after createTrack has finished storing user's track
+    const saveTrack = async () => {
+        await createTrack(name, locations)
+        reset()
+        navigate('TrackList')
     }
 
     return [saveTrack]
